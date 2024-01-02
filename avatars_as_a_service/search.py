71a3b90410df2caaeb4c63d2f9adea5859050d7d
Lewis Munyi <lewismunyiii@gmail.com>
2024-01-03 00:23:26 +0300

    Todo's

--- avatars_as_a_service/search.py
+++ avatars_as_a_service/search.py
@@ -11,21 +11,29 @@ def search_dall_e(avatar: AvatarSchema, cache=True) -> AvatarResult:
         return result
 
     if cache:
-        try:
-            write_to_cache(result)
-        except:
+        if not write_to_cache(result):
             print("Unable to cache result")
-        pass
 
     return result
 
-def write_to_cache(result: AvatarResult):
+def write_to_cache(result: AvatarResult) -> bool:
     # Todo: Attempt to write result to cache
-    pass
+    # This method should write an Avatar model to the table. It takes an AvatarResult object nad returns tru if it writes it
+    # successfully and false if it does not
+    # Find the DB design here: https://lewismunyi.hashnode.dev/preview/658af8db7aed99cfccfa584f#heading-enhanced-database-design
+    try:
+        pass
+    except:
+        print("Error accessing the DB")
+
+    return False
 
 
 def search_cache(avatar: AvatarSchema, db: Session, skip: int = 0, limit: int = 100) -> AvatarResult or None:
     # Todo: Search cache and convert to AvatarResult or None object
+    # This function should get an AvatarSchema object and search for its hash = avatar.hash_avatar() in the database/aaas.sqlite.db database.
+    # If found, it should get the record and generate an AvatarResult object(id, created_at, image_hash, image_url), else it should return null
+    # Find the DB design here: https://lewismunyi.hashnode.dev/preview/658af8db7aed99cfccfa584f#heading-enhanced-database-design
     print(db.query(Avatar).offset(skip).limit(limit).all())
     return None
 
@@ -35,13 +43,13 @@ def avatar_search(request: AvatarRequest) -> AvatarResponse:
     cache_hit: bool = False                                           # Only true if a query is returned from the DB not OpenAI
     prompt: str = request.properties.generate_prompt()
 
-    if request.disable_cache:  # Search OpenAI
+    if request.disable_cache:  # Search Dall-e
         search_result = search_dall_e(avatar=request.properties, cache=False)  # Search dall-e and don't cache the result
 
     else:  # Search cache
         query_result = search_cache(avatar=request.properties)
         if query_result is None:                                      # Cache miss
-            search_result = search_dall_e(avatar=request.properties)  # Search and attempt to cache the result
+            search_result = search_dall_e(avatar=request.properties)  # Browse image and attempt to cache the result
         else:                                                         # Cache hit
             cache_hit = True
             search_result = query_result
