294cb110b47749b1af8a42f36e7b668417e1f8a3
Agatha Bahati <matinodev@gmail.com>
2024-01-04 20:57:13 +0300

    feat: modify search endpoint

--- avatars_as_a_service/search.py
+++ avatars_as_a_service/search.py
@@ -4,29 +4,30 @@ from avatars_as_a_service.models import Avatar
 from avatars_as_a_service.schemas import Avatar as AvatarSchema, AvatarResult, AvatarRequest, AvatarResponse
 
 
-def search_dall_e(avatar: AvatarSchema, cache=True) -> AvatarResult:
+def search_dall_e(avatar: AvatarSchema, db: Session, cache=True) -> AvatarResult:
     result = avatar.dall_e_2_search()
 
     if avatar.description is not None:  # Directly search for description without caching
         return result
 
     if cache:
-        if not write_to_cache(result):
+        if not write_to_cache(result, db):
             print("Unable to cache result")
 
     return result
 
-def write_to_cache(result: AvatarResult) -> bool:
+def write_to_cache(result: AvatarResult, db: Session) -> bool:
     # Todo: Attempt to write result to cache
     # This method should write an Avatar model to the table. It takes an AvatarResult object nad returns tru if it writes it
     # successfully and false if it does not
     # Find the DB design here: https://lewismunyi.hashnode.dev/preview/658af8db7aed99cfccfa584f#heading-enhanced-database-design
     try:
-        pass
-    except:
-        print("Error accessing the DB")
-
-    return False
+        result = db.query(Avatar).filter(Avatar.image_hash == result.image_hash).first()
+        if result is None:
+            db.add(Avatar(**result.dict()))
+        return True
+    except Exception as e:
+        raise RuntimeError("An unexpected error occurred: " + str(e))    
 
 
 def search_cache(avatar: AvatarSchema, db: Session, skip: int = 0, limit: int = 100) -> AvatarResult or None:
@@ -38,18 +39,18 @@ def search_cache(avatar: AvatarSchema, db: Session, skip: int = 0, limit: int = 
     return None
 
 
-def avatar_search(request: AvatarRequest) -> AvatarResponse:
-    search_result: AvatarResult
-    cache_hit: bool = False                                           # Only true if a query is returned from the DB not OpenAI
-    prompt: str = request.properties.generate_prompt()
+def avatar_search(request: AvatarRequest, db: Session) -> AvatarResponse:
+    # search_result: AvatarResult
+    # cache_hit: bool = False                                           # Only true if a query is returned from the DB not OpenAI
+    # prompt: str = request.properties.generate_prompt()
 
     if request.disable_cache:  # Search Dall-e
-        search_result = search_dall_e(avatar=request.properties, cache=False)  # Search dall-e and don't cache the result
+        search_result = search_dall_e(avatar=request.properties, db=db, cache=False)  # Search dall-e and don't cache the result
 
     else:  # Search cache
         query_result = search_cache(avatar=request.properties)
         if query_result is None:                                      # Cache miss
-            search_result = search_dall_e(avatar=request.properties)  # Browse image and attempt to cache the result
+            search_result = search_dall_e(avatar=request.properties, db=db)  # Browse image and attempt to cache the result
         else:                                                         # Cache hit
             cache_hit = True
             search_result = query_result
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from avatars_as_a_service.database.connection import get_db
 from avatars_as_a_service.search import avatar_search
 from avatars_as_a_service.schemas import AvatarRequest, AvatarResponse
 
@@ -10,5 +11,5 @@ async def root():
 
 
 @app.post("/query")
-def search(req: AvatarRequest) -> AvatarResponse:
-    return avatar_search(request=req)
+def search(req: AvatarRequest, db: Depends(get_db)) -> AvatarResponse:
+    return avatar_search(request=req, db=db)
